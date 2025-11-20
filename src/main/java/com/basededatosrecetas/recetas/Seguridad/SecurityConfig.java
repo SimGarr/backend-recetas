9d187870f486619d57a168b1de40b423700725cb
SimGarr <sim.garrido2002@gmail.com>
2025-11-20 17:56:23 -0300

    siksi

--- src/main/java/com/basededatosrecetas/recetas/Seguridad/SecurityConfig.java
+++ src/main/java/com/basededatosrecetas/recetas/Seguridad/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,6 +48,7 @@ public class SecurityConfig {
                     "/api/archivos/**",
                     "/error"
                 ).permitAll()
+                            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 
                 // ==================== ENDPOINTS DE SUBIDA (AUTENTICADOS) ====================
                 .requestMatchers(
